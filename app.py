ceb7604c912b44a750b908a20b6586986dccb1d9
grumpy <grumpy@localhost.localdomain>
2020-12-19 16:51:32 -0600

    Added topic choice, worked on adding javascript to handle the quiz

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from os import listdir
+import os
 import csv
 import random
 
@@ -9,21 +9,39 @@ app.secret_key = b'CHANGEME!!!!!!!!!!!!'
 
 @app.route('/')
 def index():
-    quiz_choices = listdir('./csv_files')
-    return render_template('index.html', quiz_choices=quiz_choices)
+    topic_choices = []
+    dirs = os.listdir('./csv_files')
+    for _dir in dirs:
+        if os.path.isdir(f'./csv_files/{_dir}'):
+            topic_choices.append(_dir)
+    return render_template('index.html', topic_choices=topic_choices)
+
+
+@app.route('/topic')
+def topic():
+    topic_choice = request.args.get('topic')
+    quiz_choices = []
+    if topic_choice is not None:
+        dirs = os.listdir(f'./csv_files/{topic_choice}')
+        quiz_choices = dirs
+    return render_template('topic.html', quiz_choices=quiz_choices, topic_choice=topic_choice)
 
 
 @app.route('/quiz')
 def quiz():
     quiz_choice = request.args.get('quiz')
+    topic_choice = request.args.get('topic')
     rows = []
-    if quiz_choice is not None:
-        with open(f'./csv_files/{quiz_choice}') as f:
-            csvreader = csv.reader(f)
-            for row in csvreader:
-                rows.append(row)
+    if request.args.get('rows') is None:
+        if quiz_choice is not None:
+            with open(f'./csv_files/{topic_choice}/{quiz_choice}') as f:
+                csvreader = csv.reader(f)
+                for row in csvreader:
+                    rows.append(row)
+    else:
+        rows = request.args.get('rows')
     random_row = random.choice(rows)
-    return render_template('quiz.html', quiz_choice=quiz_choice, rows=rows, random_row=random_row)
+    return render_template('quiz.html', topic_choice=topic_choice, quiz_choice=quiz_choice, rows=rows, random_row=random_row)
 
 
 if __name__ == "__main__":
